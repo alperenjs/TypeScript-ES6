cdc9b01ad25002caa5f43ddc82e026e18c811494
alperenkarate <alperensozen96@gmail.com>
2021-10-03 22:20:40 +0300

    typescript introduction.

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+//Basic Types
+let id;
+let company = 'Alperen Media';
+let isPublished = true;
+let x = 'hello';
+let ids = [1, 2, 3];
+let arr = [1, true, 'hello'];
+//Tuple > specific array data types
+let person = [1, 'Alperen', true];
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+//Basic Types
+let id: number;
+let company: string = 'Alperen Media';
+let isPublished: boolean = true;
+let x: any = 'hello';
+
+let ids: number[] = [1, 2, 3];
+let arr: any[] = [1, true, 'hello'];
+
+//Tuple > specific array data types
+let person: [number, string, boolean] = [1, 'Alperen', true];
