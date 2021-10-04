74c7fa4459c05bd4dee10258a003984cdc8f2211
alperenkarate <alperensozen96@gmail.com>
2021-10-04 23:27:38 +0300

    object type determination

--- dist/index.js
+++ dist/index.js
@@ -8,3 +8,32 @@ let ids = [1, 2, 3];
 let arr = [1, true, 'hello'];
 //Tuple > specific array data types
 let person = [1, 'Alperen', true];
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+];
+let productID;
+productID = 2;
+productID = '2';
+//Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction1.Left); //3
+console.log(Direction2.Left); //Left
+const user = {
+    id: 1,
+    name: 'john'
+};
--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,51 @@ let arr: any[] = [1, true, 'hello'];
 
 //Tuple > specific array data types
 let person: [number, string, boolean] = [1, 'Alperen', true];
+
+let employee: [number, string][];
+
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+]
+
+let productID: string | number;
+productID = 2;
+productID = '2';
+
+//Enum
+enum Direction1 {
+    Up=1,
+    Down,
+    Left,
+    Right
+}
+
+enum Direction2 {
+    Up = 'Up',
+    Down = 'Down',
+    Left = 'Left',
+    Right = 'Right'
+}
+
+console.log(Direction1.Left) //3
+console.log(Direction2.Left) //Left
+
+
+//Objects
+
+// const user: {
+//     id: number,
+//     name: string
+// } = {
+//     id: 1,
+//     name: 'john'
+// }
+type User = {
+    id: number,
+    name: string
+}
+const user: User= {
+    id: 1,
+    name: 'john'
+}
